13ead5141208f2bb70c599f38463427384ce2d09
Franciszek Janicki <franciszek.janicki1@gmail.com>
2025-02-28 23:01:26 +0100

    initial commit

---
+++ Core/Src/main.cpp
@@ -0,0 +1,21 @@
+#include "main.h"
+#include "gpio.h"
+#include "i2c.h"
+#include "main.h"
+#include "system_clock.h"
+#include "usart.h"
+
+int main()
+{
+    HAL_Init();
+    SystemClock_Config();
+
+    MX_GPIO_Init();
+    MX_USART2_UART_Init();
+    MX_I2C1_Init();
+
+    while (true) {
+    }
+
+    return 0;
+}
